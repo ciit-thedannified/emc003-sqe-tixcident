22feae1a01fee7f2daab6d331dfabaf5fcd524d2
jcarlo130 <jan.pastor@ciit.edu.ph>
2024-09-23 23:21:04 +0800

    Incident Form (Initial Draft)

---
+++ src/pages/incidentForm.jsx
@@ -0,0 +1,15 @@
+import React, {useState} from "react";
+
+// stylings
+import  "./incidentForm.css";
+
+export default function incidentForm() {
+    return(
+        <section className="IFBody">
+            <div className="IFContainer">
+                <h1>Tixcident</h1>
+                <p>Incident Form Page</p>
+            </div>
+        </section>
+    )
+};
