7e5f18633afff5b973301ff54c7fc7ff666bb879
Kyoko <108702599+KyoMatias@users.noreply.github.com>
2024-09-24 22:31:35 +0800

    Design On Login Page [Not Finished, But Potential Design Is In Place, Not Functional Animations]

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,7 +1,18 @@
 import React, {useState} from "react";
-
 //Imports the Href component for the login Page css.
 import "./login.css"; 
+
+import LogoBox from '../images/TXIDENT.png'
+import BackgroundLR from '../images/LgRgBG.png'
+
+//Font Families
+<link href="https://fonts.googleapis.com/css2?family=Martian+Mono:wght@200&display=swap" rel="stylesheet"></link>;
+<link href="https://fonts.googleapis.com/css2?family=Rubik&display=swap" rel="stylesheet"></link>;
+<link href="https://fonts.googleapis.com/css2?family=Kanit:wght@200&family=Rubik&display=swap" rel="stylesheet"></link>;
+<link href="https://fonts.googleapis.com/css2?family=PT+Mono&display=swap" rel="stylesheet"></link>;
+<link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&amp;display=swap" rel="stylesheet"></link>;
+<link href="https://fonts.googleapis.com/css2?family=Epilogue:ital,wght@0,100;0,200;0,300;0,400;0,600;0,700;1,500&amp;display=swap" rel="stylesheet"></link>;
+<link href="https://fonts.googleapis.com/css2?family=Archivo:wght@100;200;300;400;500;600;700&amp;display=swap" rel="stylesheet"></link>;
 
 //Login Function that  handles the login functionality.
 
@@ -18,11 +29,10 @@ const handleSubmit = (e) => {
 };
 return (
     <div className="login-page">
-
+    
         <div className="login-container"> 
-            <div className="login-box">
-                
-                <img className="LogoBox"></img>
+            <div className="login-box"> 
+                <img className="LogoBox" src={LogoBox} alt = "LogoBox Icon"/>
                     <form onSubmit={handleSubmit}>
                         <div className="input-group">
                             <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername (e.target.value)} required/>
@@ -31,7 +41,7 @@ return (
                             <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword (e.target.value)} required/>   
                         </div>
                         <button type="submit" className="login-btn">
-                            Login
+                            LOGIN
                         </button>
                     </form>
             <div className="links">
@@ -42,6 +52,7 @@ return (
             </div>
         </div>
     </div>
+        
  );
 };
 export default Login;
