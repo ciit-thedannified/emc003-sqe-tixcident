dab3ed10222e0c4d3948a8a012cedb1e2d18afe2
Danne Uriel Boiser <danne.boiser@ciit.edu.ph>
2024-09-28 19:04:52 +0800

    Added firebase config and test files

---
+++ src/data/constants/templates/firebase_constants.js
@@ -0,0 +1,31 @@
+/*
+ * ./DATA/CONSTANTS/firebase_constants.js
+ *
+ * 'firebase_constants' contains all string constants for the application's
+ * Firebase SDK configuration, which are sensitive data of the project.
+ *
+ * This template serves as a standardized format of setting up these constants
+ * to be used for any Firebase-related operations.
+ *
+ * It is strongly advised to ONLY create a copy of this file template, move to
+ * another directory, preferably to its class folder, and enter correct
+ * value to each constant to keep the application working.
+ */
+
+const FIREBASE_API_KEY = "";
+const FIREBASE_AUTH_DOMAIN = "";
+const FIREBASE_PROJECT_ID = "";
+const FIREBASE_STORAGE_BUCKET = "";
+const FIREBASE_MESSAGING_SENDER_ID = "";
+const FIREBASE_APP_ID = "";
+const FIREBASE_MEASUREMENT_ID = "";
+
+export {
+    FIREBASE_API_KEY,
+    FIREBASE_AUTH_DOMAIN,
+    FIREBASE_PROJECT_ID,
+    FIREBASE_STORAGE_BUCKET,
+    FIREBASE_MESSAGING_SENDER_ID,
+    FIREBASE_APP_ID,
+    FIREBASE_MEASUREMENT_ID,
+};
