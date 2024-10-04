8205a7cb797e28e9112f7ebbc443141d9df47531
jcarlo130 <jan.pastor@ciit.edu.ph>
2024-10-05 01:58:53 +0800

    navbar design update (draft)

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -4,16 +4,18 @@ import React from 'react'
 import './nav.css'
 
 /* assets */
-
+import create_icn from '../assets/create.svg'
+import notif_icn from '../assets/notif.svg'
+import logout_icn from '../assets/logout.svg'
 
 function Navbar() {
   return (
     <nav>
       <h1>TIXIDENT</h1>
         <menu>
-            <li>create</li>
-            <li>notifs</li>
-            <li>logout</li>
+            <li><img src={create_icn} alt='create icon' id='create_icn' className='notif_icn' />Create</li>
+            <li><img src={notif_icn} alt='notification icon' id='notif_icn' className='notif_icn' /> Notifications</li>
+            <li><img src={logout_icn} alt='logout icon' id='logout_icn' className='notif_icn' /> Logout</li>
         </menu>
     </nav>
   )
