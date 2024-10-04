a4053cb0790e370f3eb1cf8d548e36e990ec79b8
Danne Uriel Boiser <danne.boiser@ciit.edu.ph>
2024-10-05 00:14:49 +0800

    Created authentication context and service files.

---
+++ src/services/authentication/authServices.js
@@ -0,0 +1,46 @@
+import {
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signOut,
+
+} from 'firebase/auth';
+
+import {
+    Authentication
+} from "../../configurations/firebase.config.js";
+
+/**
+ * Registers the new user and saves their account credentials to Firebase Authentication's database.
+ * As of writing, the default registration process only requires
+ *
+ * @param email User's provided e-mail credential
+ * @param password User's provided password credential
+ * @returns {Promise<UserCredential>}
+ */
+const createUser = async (email, password) => {
+    return createUserWithEmailAndPassword(Authentication, email, password);
+}
+
+/**
+ * Signs in an existing user using their valid account credentials.
+ * @param email User's provided e-mail credential
+ * @param password User's provided password credential
+ * @returns {Promise<UserCredential>}
+ */
+const loginUser = async (email, password) => {
+    return signInWithEmailAndPassword(Authentication, email, password);
+}
+
+/**
+ * Signs out the currently authenticated user from the system.
+ * @returns {Promise<void>}
+ */
+const logoutUser = async () => {
+    return signOut(Authentication);
+}
+
+export {
+    createUser,
+    loginUser,
+    logoutUser,
+}
