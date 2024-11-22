b8dfc8a1aa18e49ff6b151c9cc4bfd7213d6ede0
Danne Uriel Boiser <danne.boiser@ciit.edu.ph>
2024-11-22 21:45:40 +0800

    Removed Bootstrap CSS dependencies; added Tailwind CSS & Fontawesome.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import React from "react";
-import Login from './pages/login';
 import Register from "./pages/register";
 
 function App(){
   return (
     <Register/>
   );
-};
+}
 
 export default App;
