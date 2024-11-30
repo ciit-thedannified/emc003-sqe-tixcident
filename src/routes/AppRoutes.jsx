13060578ebb96ac288c727617dcd9c88f1b1001b
Danne Uriel Boiser <danne.boiser@ciit.edu.ph>
2024-12-01 02:53:10 +0800

    Added admin and form routing in AppRoutes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,18 +1,26 @@
 import {createBrowserRouter} from "react-router-dom";
-import App from "../App.jsx";
-import ErrorBoundarySample from "../pages/errors/ErrorBoundarySample.jsx";
 import AuthlessRoute from "../components/authentication/AuthlessRoute.jsx";
 import LoginPage from "../pages/auth/LoginPage.jsx";
 import SignUpPage from "../pages/auth/SignUpPage.jsx";
-import AuthenticatedRoute from "../components/authentication/AuthenticatedRoute.jsx";
 import ClientDashboard from "../pages/client/ClientDashboard.jsx";
 import ClientDashboardHomePage from "../pages/client/screens/ClientDashboardHomePage.jsx";
 import ClientDashboardTicketsPage from "../pages/client/screens/ClientDashboardTicketsPage.jsx";
 import ClientDashboardViewTicketPage from "../pages/client/screens/ClientDashboardViewTicketPage.jsx";
 import ClientDashboardFeedbacksPage from "../pages/client/screens/ClientDashboardFeedbacksPage.jsx";
 import ClientDashboardViewFeedbackPage from "../pages/client/screens/ClientDashboardViewFeedbackPage.jsx";
+import AdminDashboard from "../pages/admin/AdminDashboard.jsx";
+import AdminDashboardHomePage from "../pages/admin/screens/AdminDashboardHomePage.jsx";
+import AdminDashboardViewFeedbackPage from "../pages/admin/screens/AdminDashboardViewFeedbackPage.jsx";
+import AdminDashboardFeedbacksPage from "../pages/admin/screens/AdminDashboardFeedbacksPage.jsx";
+import AdminDashboardIssuesPage from "../pages/admin/screens/AdminDashboardIssuesPage.jsx";
+import AdminDashboardViewIssuePage from "../pages/admin/screens/AdminDashboardViewIssuePage.jsx";
+import AdminDashboardUsersPage from "../pages/admin/screens/AdminDashboardUsersPage.jsx";
 
 function FeedbackFormPage() {
+    return null;
+}
+
+function IssueFormPage() {
     return null;
 }
 
@@ -67,12 +75,12 @@ const AppRoutes = createBrowserRouter([
                     <ClientDashboardViewTicketPage />
                 )
             },
-/*            {
+            {
                 path: '/issues/create',
                 element: (
                     <IssueFormPage />
                 ),
-            },*/
+            },
             {
                 path: 'feedbacks',
                 element: (
@@ -91,8 +99,64 @@ const AppRoutes = createBrowserRouter([
                     <FeedbackFormPage />
                 )
             },
+            {
+                path: 'feedbacks/create',
+                element: (
+                    <FeedbackFormPage />
+                )
+            },
         ]
     },
+    {
+        path: '/a',
+        element: (
+            <AdminDashboard />
+        ),
+        children: [
+            {
+                index: true,
+                element: (
+                    <AdminDashboardHomePage />
+                ),
+            },
+            {
+                path: 'issues',
+                element: (
+                    <AdminDashboardIssuesPage />
+                ),
+            },
+            {
+                path: 'issues/:issue_id',
+                element: (
+                    <AdminDashboardViewIssuePage />
+                ),
+            },
+            {
+                path: 'issues/create',
+                element: (
+                    <IssueFormPage />
+                )
+            },
+            {
+                path: 'feedbacks',
+                element: (
+                    <AdminDashboardFeedbacksPage />
+                ),
+            },
+            {
+                path: 'feedbacks/:feedback_id',
+                element: (
+                    <AdminDashboardViewFeedbackPage />
+                ),
+            },
+            {
+                path: 'users',
+                element: (
+                    <AdminDashboardUsersPage />
+                ),
+            },
+        ]
+    }
 ]);
 
 export default AppRoutes;
