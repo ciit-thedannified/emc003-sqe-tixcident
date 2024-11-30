da2a21cc8cf24e1575422a001ed1bd23a3bef114
Danne Uriel Boiser <danne.boiser@ciit.edu.ph>
2024-11-30 21:42:02 +0800

    Adjustment in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,14 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from 'react-dom/client';
+import './assets/css/index.css';
 import {RouterProvider} from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes.jsx";
-import {AuthProvider} from "./contexts/AuthContext.js";
-import {AxiosProvider} from "./contexts/AxiosContext.js";
+import {AuthProvider} from "./contexts/AuthContext.jsx";
+import {AxiosProvider} from "./contexts/AxiosContext.jsx";
 
 createRoot(document.getElementById('root')).render(
     <AuthProvider>
         <AxiosProvider>
-            <RouterProvider router={AppRoutes}>
-                <App />
-            </RouterProvider>
+            <RouterProvider router={AppRoutes} />
         </AxiosProvider>
     </AuthProvider>
 )
