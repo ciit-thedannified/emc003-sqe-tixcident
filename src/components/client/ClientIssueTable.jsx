e42b299588c68afd025ede3362e72c3f4f5b44e7
Danne Uriel Boiser <danne.boiser@ciit.edu.ph>
2024-12-01 11:58:23 +0800

    Modified client issue table with custom cell rendering, renamed from ClientTable to ClientIssueTable

--- src/components/client/ClientIssueTable.jsx
+++ src/components/client/ClientIssueTable.jsx
@@ -1,38 +1,99 @@
-import { useReactTable, getCoreRowModel, flexRender, getFilteredRowModel, getSortedRowModel } from "@tanstack/react-table";
+import {
+    flexRender,
+    getCoreRowModel,
+    getFilteredRowModel,
+    getSortedRowModel,
+    useReactTable
+} from "@tanstack/react-table";
 import datas from "../../data/tixcident.issues.json";
-import { useMemo, useState } from "react";
+import {useMemo, useState} from "react";
+import {EyeIcon} from "@heroicons/react/24/solid/index.js";
+import {useNavigate} from "react-router-dom";
+import {DateTime} from "luxon";
+import {IssueTypes, PriorityTypes, StatusTypes} from "../../data/enums.js";
 
-export default function AdminTable() {
+export default function ClientIssueTable() {
+    const navigate = useNavigate();
     const data = useMemo(() => datas, []);
 
     const columns = [
         { header: "Title", accessorKey: "title" },
-        { header: "Status", accessorKey: "status" },
-        { header: "Staff", accessorKey: "staff" },
-        { header: "Priority", accessorKey: "priority" },
-        { header: "Type", accessorKey: "type" },
-        { header: "Created at", accessorKey: "createdAt" },
-        { header: "Updated at", accessorKey: "updatedAt" },
+        {
+            header: "Status",
+            accessorKey: "status",
+            cell: info => {
+                return Object.values(StatusTypes).find(s => s.value === info.getValue()).label;
+            }
+        },
+        { header: "Staff", accessorKey: "staff", cell: info => {
+                return info.getValue() ?? "None";
+            }
+        },
+        {
+            header: "Priority",
+            accessorKey: "priority",
+            cell: info => {
+                return Object.values(PriorityTypes).find(p => p.value === info.getValue()).label;
+            }
+        },
+        {
+            header: "Type",
+            accessorKey: "type",
+            cell: info => {
+                return Object.values(IssueTypes).find(i => i.value === info.getValue()).label;
+            }
+        },
+        { header: "Created at", accessorKey: "createdAt", cell: info => {
+                return DateTime.fromISO(info.getValue()['$date']).toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS) ?? "None";
+            }
+        },
+        { header: "Updated at", accessorKey: "updatedAt", cell: info => {
+                return DateTime.fromISO(info.getValue()['$date']).toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS) ?? "None";
+            }
+        },
+        { header: "Actions", accessorKey: "_id", cell: info => {
+            const issue_id = info.getValue()["$oid"];
+
+            return (
+                    <div className="flex space-x-5">
+                        <button onClick={() => {
+                            navigate(`/u/issues/${issue_id}`);
+                        }}>
+                            <EyeIcon className="w-6 h-6" />
+                        </button>
+                    </div>
+                );
+            }
+        }
     ];
 
-   
+
     const [filtering, setFiltering] = useState('')
     const [sorting, setSorting] = useState([])
 
-    const table = useReactTable({ data, columns, getCoreRowModel: getCoreRowModel(), getFilteredRowModel: getFilteredRowModel(), getSortedRowModel: getSortedRowModel(), 
-        state:{globalFilter: filtering,
-                sorting: sorting,
+    const table = useReactTable({
+        data,
+        columns,
+        getCoreRowModel: getCoreRowModel(),
+        getFilteredRowModel: getFilteredRowModel(),
+        getSortedRowModel: getSortedRowModel(),
+        state: {
+            globalFilter: filtering,
+            sorting: sorting,
         },
-        
+
         onGlobalFilterChange: setFiltering,
         onSortingChange: setSorting,
-     });
+    });
 
 
     return (
-        <div className="p-6 bg-gray-100 min-h-screen">
-            <input type = "text" value={filtering} onChange={(e) => setFiltering(e.target.value)}placeholder="Search..." className="w-full p-3 mb-4 text-sm text-gray-700 bg-gray-100 
+        <div className="p-6 bg-gray-100">
+            <div className="flex">
+                Search Issue:
+                <input type = "text" value={filtering} onChange={(e) => setFiltering(e.target.value)}placeholder="Search..." className="w-full p-3 mb-4 text-sm text-gray-700 bg-gray-100
             border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"/>
+            </div>
             {/* Wrapper to enforce full-width */}
             <div className="overflow-x-auto w-full border border-gray-200 bg-white shadow-md rounded-lg">
                 {/* Table with enforced full-width */}
